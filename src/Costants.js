bbadfcb2f48dc09393a255ca40c3bf87c530ca8c
eziosantori <jotartaglia82@gmail.com>
2022-09-01 21:45:55 +0200

    Some Clean

---
+++ src/Costants.js
@@ -0,0 +1,10 @@
+// Some clean code
+export const ITEMS = {
+  AGEDBRIE: 'Aged Brie',
+  BACKSTAGEPASSES: 'Backstage passes to a TAFKAL80ETC concert',
+  SULFURAS: 'Sulfuras, Hand of Ragnaros',
+  DEXTERITYVEST: '+5 Dexterity Vest',
+  ELIXIRMONGOSE: 'Elixir of the Mongoose',
+};
+
+export const MAXQ = 50;
